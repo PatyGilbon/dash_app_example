1b6411a398bbfe5255c521020940466333738f7e
Paty Gilbon <anapatygilbon@gmail.com>
2017-12-11 15:56:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import pandas as pd
 app = dash.Dash(__name__)
 server = app.server
 
-df = pd.read_csv('/Users/patygilbon/Downloads/nama_10_gdp/nama_10_gdp_1_Data.csv')
+df = pd.read_csv('https://github.com/PatyGilbon/dash_app_example/blob/master/nama_10_gdp_1_Data.csv')
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 available_indicators = df['UNIT'].unique()
 value= df['Value'].unique()
